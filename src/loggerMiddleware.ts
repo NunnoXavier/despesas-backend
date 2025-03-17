2ace0be7df8de55831d69fde175e29814507507a
Nuno Xavier <nuno@192.168.0.23>
2025-03-17 14:26:39 -0300

    ajustes no middleware que define as origens permitidas, para evitar problemas de CORS

--- src/loggerMiddleware.ts
+++ src/loggerMiddleware.ts
@@ -1,8 +1,13 @@
 import { Request, Response, NextFunction } from 'express'
 
+const allowedOrigins = ["http://localhost:5173","http://localhost:3000"]
+
 const loggerMiddleware = (req:Request, res:Response, next:NextFunction): void => {  
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+    const origin = req.headers.origin;
+    if (origin && allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   
